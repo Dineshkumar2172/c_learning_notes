4a6917aedd042fd44143771c58e159d28eb9adbe
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-08-16 19:30:54 +0530

    completed till module 8 - c bootcamp

---
+++ c_learning_bootcamp/comments_and_format_specification/comments.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    /* MULTILINE COMMENTS GOES HERE */
+    // ONE LINER COMMENTS GOES HERE
+    return 0;
+}
---
+++ c_learning_bootcamp/comments_and_format_specification/format_specification.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+        %d - is for integers numbers.
+        %f - is for floar numbers.
+        %.nf - repalce n with the number of floating points you wanna display in float.
+    */
+    printf("I am %d years old\n", 20); // %id is one of the format specifier used to hold place for integer number.
+    printf("Today I am %d years old and next year I'm going to be %d years old\n", 20, 21);
+    printf("My average grade : %f\n", 93.7);
+    printf("My average grade : %.1f\n", 93.7);
+
+    // milestone #1 - building your first calculator A
+    printf("addition : %d\n", 3+3);
+    printf("subtraction : %d\n", 5-3);
+    printf("multiplication : %.2f\n", 5*12.2);
+
+    // by default, c does integer division if both the divider values are integers. use (double) or make one of the either values as float.
+    // printf("dividing directly will throw error : %.2f\n", 15/2);
+    printf("division (using double keyword) : %.2f\n", (double)15/2);
+    printf("division (making either of one values as float) : %.2f\n", 15.0/2);
+    printf("remainder : %d\n", 15%2);
+
+
+    printf("calculator based on format specification : \n");
+    printf("%d + %d = %d\n", 5, 2, 5 + 2);
+    printf("%d - %d = %d\n", 5, 2, 5 - 2);
+    printf("%d * %d = %d\n", 5, 2, 5 * 2);
+    printf("%d / %d = %d\n", 5, 2, 5 / 2);
+    printf("%d %% %d = %d\n", 5, 2, 5 % 2);
+
+    // print asterisk pattern
+    printf("\n\nprinting asterisk pattern : \n");
+    printf("1*********\n");
+    printf("12********\n");
+    printf("123*******\n");
+    printf("1234******\n");
+    printf("12345*****\n");
+
+    return 0;
+}
