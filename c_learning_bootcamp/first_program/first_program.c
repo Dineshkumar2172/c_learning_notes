e661a235bb24e769041865365fd17b721cce1543
Dineshkumar2172 <dinesh2000.data@gmail.com>
2024-08-16 20:18:23 +0530

    random commit

---
+++ c_learning_bootcamp/first_program/first_program.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World!\n");
+
+    // challenge #1 - print your first name and last name!
+    printf("Dineshkumar \nAnbalagan\n");
+    printf("first name: Dineshkumar \nlast name: Anbalagan\n");
+
+    // challenge #2 - print your full name, age, and gender!
+    printf("full name: Dineshkumar Anbalagan \nage: 24 \ngender: male\n");
+
+    // challenge #3 - write a program that will print an isosceles triangle
+    // solution 1: using multiple printf commands
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n");
+
+    // solution 2: using single printf commands
+    printf("  *  \n *** \n*****\n");
+
+    return 0;
+}
---
+++ c_learning_bootcamp/first_program/introduction.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // standard input and output library, it gives all the necessary functionalities to work on inputs and outputs
+#include <stdlib.h>
+
+int main() // this here specifying the entry point
+{ // it's more like a container for the block of commands to execute - beginning of the block
+    // your code should be here
+    // your code should be here
+    // your code should be here
+    // your code should be here
+    // comments
+    return 0; // command to indicate (0 indicates) that program/code has terminated successfully
+} // end of the block
